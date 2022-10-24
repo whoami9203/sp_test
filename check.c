a693dac8f9b19ff810480125ec1298b0f856ac01
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2022-10-24 16:05:07 +0800

    Update check.c

--- check.c
+++ check.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 
 int
 main(void)
 {
 	if (open("tempfile", O_RDWR) < 0)
-		err_sys("open error");
+		exit(0);
 
 	printf("file opened\n");
 	sleep(15);
