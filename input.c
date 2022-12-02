78cd8e84c5aa3c39d488deec719665146343983d
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2022-12-02 10:13:08 +0800

    Update input.c

--- input.c
+++ input.c
@@ -10,19 +10,21 @@
 #include <sys/types.h>
 #include <sys/signal.h>
 
+#define MAX 3
+
 int main(int argc, char const *argv[])
 {
 	char path[] = "2_max_subarray";
 	int fd;
 	int num;
-	char buf[50];
+	char buf[MAX][50] = {"1111\n", "   1\n", " 123\n"};
 
-	fd = open(path, O_WRONLY|O_NONBLOCK);
+	//fd = open(path, O_WRONLY|O_NONBLOCK);
 	
-	while(gets(buf) != NULL){
+	for(int i=0; i<MAX; i++){
 
-		printf("buf: %s\n", buf);
-		write(fd, buf, strlen(buf));
+		printf("buf: %s", buf[i]);
+		//write(fd, buf, strlen(buf));
 
 		sleep(1);
 	}
