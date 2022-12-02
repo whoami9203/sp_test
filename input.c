d877673bce371633d288224837e3c061c796ca20
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2022-12-02 17:18:14 +0800

    Update input.c

--- input.c
+++ input.c
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[])
 	char path1[] = "2_max_subarray";
 	char path2[] = "3_max_subarray";
 	char buf1[MAX1][50] = {"   2\n   4\n  -3\n", "  -2\n", "  10\n"};
-	char buf2[MAX2][50] = {"   13\n  -5\n  -7\n", "   9\n", "   8\n", "  -1\n"};
+	char buf2[MAX2][50] = {"  13\n  -5\n  -7\n", "   9\n", "   8\n", "  -1\n"};
 	int fd1, fd2;
 	pid_t pid1, pid2;	
 
@@ -32,7 +32,7 @@ int main(int argc, char const *argv[])
 
 		for(int i=0; i<MAX1; i++){
 
-			printf("buf1: %s\n", buf1[i]);
+			printf("buf1: %s", buf1[i]);
 			write(fd1, buf1[i], strlen(buf1[i]));
 
 			sleep(4);
@@ -45,7 +45,7 @@ int main(int argc, char const *argv[])
 
 			for(int i=0; i<MAX2; i++){
 
-				printf("buf2: %s\n", buf2[i]);
+				printf("buf2: %s", buf2[i]);
 				write(fd2, buf2[i], strlen(buf2[i]));
 
 				sleep(4);
