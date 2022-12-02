aec3e53bb41e95369e0a9165a3e739100d7be1f0
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2022-12-02 16:54:13 +0800

    Update input.c

--- input.c
+++ input.c
@@ -6,30 +6,57 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <errno.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/signal.h>
 
-#define MAX 2
+#define MAX1 3
+#define MAX2 4
 #define count 5
 
 int main(int argc, char const *argv[])
 {
-	char path[] = "2_max_subarray";
-	int fd;
-	int num;
-	char buf[MAX][50] = {"   2\n   4\n  -3\n", " 197\n"};
+	char path1[] = "2_max_subarray";
+	char path2[] = "3_max_subarray";
+	char buf1[MAX][50] = {"   2\n   4\n  -3\n", "  -2\n", "  10\n"};
+	char buf1[MAX][50] = {"   13\n  -5\n  -7\n", "   9\n", "   8\n", "  -1\n"};
+	int fd1, fd2;
+	pid_t pid1, pid2;	
 
-	fd = open(path, O_WRONLY|O_NONBLOCK);
 	
-	for(int i=0; i<MAX; i++){
 
-		printf("buf: %s\n", buf[i]);
-		write(fd, buf[i], strlen(buf[i]));
+	pid1 = fork();
+	if(pid1 == 0){
+		fd1 = open(path1, O_WRONLY|O_NONBLOCK);
 
-		sleep(11);
+		for(int i=0; i<MAX1; i++){
+
+			printf("buf1: %s\n", buf1[i]);
+			write(fd1, buf1[i], strlen(buf1[i]));
+
+			sleep(4);
+		}
+	}
+	else{
+		pid2 = fork();
+		if(pid2 == 0){
+			fd2 = open(path2, O_WRONLY|O_NONBLOCK);
+
+			for(int i=0; i<MAX2; i++){
+
+				printf("buf2: %s\n", buf2[i]);
+				write(fd2, buf2[i], strlen(buf2[i]));
+
+				sleep(4);
+			}
+		}
+		else{
+			waitpid(pid1, NULL, 0);
+			waitpid(pid2, NULL, 0);
+		}
 	}
 	
-
-	return 0;
+	
+	exit(0);
 }
