42de8ac6ae70196a55e752c60857ec6a013f335f
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2022-12-02 13:57:05 +0800

    Update input.c

--- input.c
+++ input.c
@@ -11,6 +11,7 @@
 #include <sys/signal.h>
 
 #define MAX 3
+#define count 5
 
 int main(int argc, char const *argv[])
 {
@@ -24,7 +25,7 @@ int main(int argc, char const *argv[])
 	for(int i=0; i<MAX; i++){
 
 		printf("buf: %s\n", buf[i]);
-		write(fd, buf, strlen(buf[i]));
+		write(fd, buf, count);
 
 		sleep(3);
 	}
