0c15120544668466d277ec83ef53ec89059e27d4
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2022-12-15 22:26:51 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,36 @@
+pathans="./answers"
+
+echo ""
+
+for i in {1..5};do 
+    make &>/dev/null all
+    echo "------------ Test case $i ------------"
+    time ./tserver < ./testcases/input$i.txt
+    time ./pserver < ./testcases/input$i.txt
+    echo "------------ Diff check -------------"
+    echo "(empty = no difference)"
+    for i in {1..170000}; do
+        if [ -f "./${i}t.out" ]; then
+            diff -q ./${i}t.out ./answers/${i}s.out
+        fi
+        if [ -f "./${i}p.out" ]; then
+            diff -q ./${i}p.out ./answers/${i}s.out
+        fi
+    done
+    echo ""
+    make &>/dev/null clean
+done
+
+
+# for i in {1..170000}; do
+#     if [ -f "./${i}t.out" ]; then
+#         diff -q ./${i}t.out ./answers/${i}s.out
+#     fi
+#     if [ -f "./${i}p.out" ]; then
+#         diff -q ./${i}p.out ./answers/${i}s.out
+#     fi
+# done
+
+echo "-------------------------------------"
+
+make clean
